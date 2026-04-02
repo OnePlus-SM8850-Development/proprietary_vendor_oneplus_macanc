51cbcdb5b6a545bf9cc93a8c8ccd689826902b67
chandu078 <chandudyavanapelli03@gmail.com>
2026-04-02 06:55:27 +0000

    macanc: Initial import proprietary files from COS 11.A.50
    Change-Id: Ibc85f545893fa70df0d97a50b49c5c5e20149c7a

---
+++ BoardConfigVendor.mk
@@ -0,0 +1,47 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    engineering_cdt \
+    featenabler \
+    hyp \
+    hyp_ac_config \
+    imagefv \
+    keymaster \
+    modem \
+    multiimgoem \
+    multiimgqti \
+    oplus_sec \
+    pdp \
+    pdp_cdb \
+    qtvm_dtbo \
+    qupfw \
+    secretkeeper \
+    shrm \
+    soccp \
+    soccp_dcd \
+    soccp_debug \
+    splash \
+    spuservice \
+    tme_config \
+    tme_fw \
+    tme_seq_patch \
+    tz \
+    tz_ac_config \
+    tz_qti_config \
+    uefi \
+    uefisecapp \
+    vm-bootsys \
+    xbl \
+    xbl_ac_config \
+    xbl_config \
+    xbl_ramdump
