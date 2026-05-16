ac9cf02e6dfa526b651f5702f3acf9967f957e56
chandu078 <chandudyavanapelli03@gmail.com>
2026-05-16 11:25:10 +0000

    macanc: Add previously decommonized DSP modules
    Change-Id: I2564016ede7b0578861f8f1378e488ec1ae22c66

--- macanc-vendor.mk
+++ macanc-vendor.mk
@@ -1503,6 +1503,34 @@ PRODUCT_PACKAGES += \
     libtfestriping \
     libubifocus \
     libvideoml \
+    libDLDPP_skel \
+    libHexlpQnnHtp \
+    libHexlpQnnSystem \
+    libVppFrcQnnHtp \
+    libapn_dsp_skel \
+    libbitml_nsp_81na_skel \
+    libbitml_nsp_skel \
+    libdspCV_skel \
+    libfastcvadsp \
+    libfastcvdsp_skel \
+    libhdr_skel \
+    libmobilenet_dsp_frc \
+    libmobilenet_dsp_frc_networks \
+    libnsp_ipc_cdsp_skel \
+    libnspextensioncopyprovider \
+    libnspextensionfrcprovider \
+    libnspextensiongenericqnnprovider \
+    libnspextensionsuperresolutionprovider \
+    libqsegnetengine_dsp_skel \
+    libscveT2T_skel \
+    libsvDescMatchImpl_dsp_skel \
+    libsvDescMatch_dsp \
+    libsvdsp \
+    libsvswdsp \
+    libsvswdsp_skel \
+    libvpp_frc \
+    libvpp_gfrc_networks \
+    libvpp_svc_skel \
     vendor.oplus.hardware.camera.aon-V1-ndk_vendor \
     vendor.oplus.hardware.cammidasservice-V1-ndk_vendor \
     vendor.oplus.hardware.cammidasservice@1.0 \
