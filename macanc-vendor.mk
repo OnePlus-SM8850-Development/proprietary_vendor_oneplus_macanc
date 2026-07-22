ac342d721fb863b813e5c20c3b073433ece6eed0
chandu078 <chandudyavanapelli03@gmail.com>
2026-07-22 08:35:55 +0000

    macanc: Remove unused HIDL cammidasservice
    Change-Id: I5094cb88980ae726e9ff734499e4eb2cca7f3c14

--- macanc-vendor.mk
+++ macanc-vendor.mk
@@ -1481,7 +1481,6 @@ PRODUCT_PACKAGES += \
     libmctfengine_stub \
     libmfGhostDetection \
     libmfec \
-    libmidasserviceintf \
     libmidasserviceintf_aidl \
     libmmcamera_bestats \
     libmmcamera_cac \
@@ -1544,7 +1543,6 @@ PRODUCT_PACKAGES += \
     libvpp_frc \
     libvpp_gfrc_networks \
     libvpp_svc_skel \
-    vendor.oplus.hardware.cammidasservice@1.0 \
     vendor.qti.hardware.camera.aon-service-impl \
     vendor.qti.hardware.camera.offlinecamera-service-impl \
     libQnnHtpV81Skel.signed.aiframe \
